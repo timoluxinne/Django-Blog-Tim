c4b8f88927e3b635ddb6b741dd081f87e27a8bff
Tim_redBlack <67490655+timoluxinne@users.noreply.github.com>
2021-03-05 17:29:11 +0300

    static files

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 import os
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 LOGIN_REDIRECT_URL = '/'
